8ad628dc25233b9d40f12b71401157ba9ce86549
bibun-queen <vj4bdgjk28@privaterelay.appleid.com>
2026-08-17 10:55:28 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  supabaseUrl: "https://tbahaqcctejyynhxjbch.supabase.co",
+  supabaseUrl: "https://tbahaqcctejyynhxjbch.supabase.co/rest/v1/",
   supabasePublishableKey: "sb_publishable_3DkEsr95r70rg11LcOjQqQ_Axz00edY",
   appName: "MedMatch",
 };
