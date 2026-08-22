1562ef9fc1e8e173c76f0435c88d14575477bbff
bibun-queen <vj4bdgjk28@privaterelay.appleid.com>
2026-08-22 19:00:27 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  supabaseUrl: "https://YOUR_PROJECT.supabase.co",
-  supabasePublishableKey: "sb_publishable_REPLACE_ME",
+  supabaseUrl: "https://tbahaqcctejyynhxjbch.supabase.co",
+  supabasePublishableKey: "sb_publishable_3DkEsr95r70rg11LcOjQqQ_Axz00edY",
   appName: "MedMatch",
 };
